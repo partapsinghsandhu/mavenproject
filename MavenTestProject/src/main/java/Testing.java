9528519a6c140ed579b10eb5eaf8e5d33485659d
partapsinghsandhu <69760352+partapsinghsandhu@users.noreply.github.com>
2020-08-17 10:29:16 +0530

    Update Testing.java
    Added void Main

--- MavenTestProject/src/main/java/Testing.java
+++ MavenTestProject/src/main/java/Testing.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Testing {
-    public void test(){
+    public static void main(String[] arg){
         System.out.println("hi");
+    Integer[] arry = {4,6,8};
+    Integer[] arrayReverse = new Integer[arry.length];
+    String[] str = {"a","b","e","c"};
+    for (int i = arry.length-1;i>=0;i--){
+        System.out.println(arry[i]);
+
     }
+     String str1 =   Arrays.toString(str);
+    Arrays.sort(str);
+    Arrays.sort(arry);
+    StringBuilder stringBuilder = new StringBuilder(str1);
+    stringBuilder.reverse();
+
+    }
+
 }
